890345fd5ea1c415ece640dee4a18f4fbda1f009
George Fayek <georgefayekwilliam@gmail.com>
2026-04-22 20:24:14 +0200

    BOOKSTORE

---
+++ G-NET-12-EF01/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using G_NET_12_EF01.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_EF01.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        // Tables
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=BookStoreDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ G-NET-12-EF01/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET_12_EF01.Models
+{
+    internal class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        // Navigation Property
+        public List<Book> Books { get; set; }
+    }
+}
